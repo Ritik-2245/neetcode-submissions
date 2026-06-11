77d83983c1efd1f860f2599e002979d5dfa8fde2
RITIK KUMAR <94675803+Ritik-2245@users.noreply.github.com>
2026-06-11 21:47:04 +0530

    Add: interleaving-string - submission-3

---
+++ Data Structures & Algorithms/interleaving-string/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+private String x,y,z;
+
+private boolean checkInterLeaving(int a,int b)
+{
+  if(a==x.length() && b==y.length())return true;
+
+boolean ans = false;
+
+if(a<x.length() && x.charAt(a)==z.charAt(a+b))
+    ans|=checkInterLeaving(a+1, b);
+
+if(b<y.length() && y.charAt(b)==z.charAt(a+b))
+    ans|=checkInterLeaving(a, b+1);
+
+return ans;
+}
+ public boolean isInterleave(String s1, String s2, String s3) {
+    
+    if(s1.length()+s2.length()!=s3.length())return false;
+
+     x=s1;
+     y=s2;
+    z=s3;
+
+return checkInterLeaving(0, 0);
+
+    }
+
+}
